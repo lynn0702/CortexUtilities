066e5487c9251373280d01249d1e6b8d4aafdb2f
Lynn Jones <lynn.jones.0702@gmail.com>
2022-02-01 20:38:13 -0600

    pre-alpha randomn scenes

---
+++ Data/CortexSceneTrait.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class CortexSceneTrait
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public CortexSceneTrait()
+        {
+
+        }
+
+        public CortexSceneTrait(IList<object> values)
+        {
+            Name = values[0].ToString();
+            Type = values[1].ToString();
+        }
+    }
+}
