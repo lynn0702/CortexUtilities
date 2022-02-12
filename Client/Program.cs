728021043cf209b78929de0612c29f9a6dc71417
Lynn Jones <lynn.jones.0702@gmail.com>
2022-02-11 19:50:08 -0600

    update for APIKey

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,6 +9,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Configuration.GetSection("googleAPIKey");
+
 builder.Services.AddScoped<GoogleAuthorizationMessageHandler>();
 
 builder.Services.AddHttpClient("GoogleSheets",
